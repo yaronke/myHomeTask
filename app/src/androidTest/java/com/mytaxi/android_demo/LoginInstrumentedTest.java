9faf0a046a06615e41de4bb961f3737641553cb7
yaronke <yaronke@gmail.com>
2018-04-28 01:10:00 +0200

    testing

--- app/src/androidTest/java/com/mytaxi/android_demo/LoginInstrumentedTest.java
+++ app/src/androidTest/java/com/mytaxi/android_demo/LoginInstrumentedTest.java
@@ -24,7 +24,7 @@ public class LoginInstrumentedTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
             MainActivity.class);
-//mytest
+
     @Test
     public void logInUserPass() {
         onView(withId(R.id.edt_username))
